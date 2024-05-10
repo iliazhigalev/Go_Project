4cbe6f9e09c7fb4c38ac0e2c9c8a6ace2d7e0010
ILIA <iliazhigalev@yandex.ru>
2024-05-10 16:46:08 +0300

    Added logic for deleting and updating a fact

--- cmd/routes.go
+++ cmd/routes.go
@@ -15,4 +15,10 @@ func setupRoutes(app *fiber.App) {
 	app.Post("/fact", handlers.CreateFact)
 
 	app.Get("/fact/:id", handlers.ShowFact)
+
+	app.Get("/fact/:id/edit", handlers.EditFact)
+
+	app.Patch("/fact/:id", handlers.UpdateFact)
+
+	app.Delete("/fact/:id", handlers.DeleteFact)
 }
